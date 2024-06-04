bcbae7283ddaf19cb7ed42aa417314ee5d496bf5
CyberianEV <eaviutskyi@gmail.com>
2024-06-04 16:31:00 +0200

    Init

---
+++ src/main/java/org/spring/hiber_second/HiberSecondApplication.java
@@ -0,0 +1,13 @@
+package org.spring.hiber_second;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiberSecondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiberSecondApplication.class, args);
+	}
+
+}
